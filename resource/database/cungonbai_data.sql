780a001c05d639def4f739b6def4c5a1d5012808
trungdq88 <trungdq88@gmail.com>
2014-10-18 13:27:57 +0000

    Add relationship to sql

--- resource/database/cungonbai_data.sql
+++ resource/database/cungonbai_data.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 18, 2014 at 02:33 PM
+-- Generation Time: Oct 18, 2014 at 03:27 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -104,7 +104,7 @@ CREATE TABLE IF NOT EXISTS `tbl_subject` (
 CREATE TABLE IF NOT EXISTS `tbl_user` (
 `user_id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `point` int(11) NOT NULL
+  `score` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
@@ -115,31 +115,31 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
 -- Indexes for table `tbl_answer`
 --
 ALTER TABLE `tbl_answer`
- ADD PRIMARY KEY (`answer_id`);
+ ADD PRIMARY KEY (`answer_id`), ADD KEY `question_id` (`question_id`);
 
 --
 -- Indexes for table `tbl_favorite`
 --
 ALTER TABLE `tbl_favorite`
- ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`), ADD KEY `user_id` (`user_id`), ADD KEY `question_id` (`question_id`);
 
 --
 -- Indexes for table `tbl_question`
 --
 ALTER TABLE `tbl_question`
- ADD PRIMARY KEY (`question_id`);
+ ADD PRIMARY KEY (`question_id`), ADD KEY `subject_id` (`subject_id`);
 
 --
 -- Indexes for table `tbl_session`
 --
 ALTER TABLE `tbl_session`
- ADD PRIMARY KEY (`session_id`);
+ ADD PRIMARY KEY (`session_id`), ADD KEY `user_id` (`user_id`), ADD KEY `subject_id` (`subject_id`);
 
 --
 -- Indexes for table `tbl_session_question`
 --
 ALTER TABLE `tbl_session_question`
- ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `session_id` (`session_id`,`question_id`,`answer_id`), ADD KEY `session_id_2` (`session_id`), ADD KEY `question_id` (`question_id`), ADD KEY `answer_id` (`answer_id`);
 
 --
 -- Indexes for table `tbl_subject`
@@ -187,6 +187,44 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 ALTER TABLE `tbl_user`
 MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_answer`
+--
+ALTER TABLE `tbl_answer`
+ADD CONSTRAINT `tbl_answer_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `tbl_question` (`question_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tbl_favorite`
+--
+ALTER TABLE `tbl_favorite`
+ADD CONSTRAINT `tbl_favorite_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `tbl_favorite_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `tbl_question` (`question_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tbl_question`
+--
+ALTER TABLE `tbl_question`
+ADD CONSTRAINT `tbl_question_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `tbl_subject` (`subject_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tbl_session`
+--
+ALTER TABLE `tbl_session`
+ADD CONSTRAINT `tbl_session_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `tbl_session_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `tbl_subject` (`subject_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tbl_session_question`
+--
+ALTER TABLE `tbl_session_question`
+ADD CONSTRAINT `tbl_session_question_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `tbl_question` (`question_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `tbl_session_question_ibfk_3` FOREIGN KEY (`answer_id`) REFERENCES `tbl_answer` (`answer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `tbl_session_question_ibfk_4` FOREIGN KEY (`session_id`) REFERENCES `tbl_session` (`session_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
