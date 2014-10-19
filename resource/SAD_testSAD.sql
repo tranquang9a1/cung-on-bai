c31bd3df646885d337146a6f96e369a38e517aa4
truongnguyenbao <truongnguyenbao@gmail.com>
2014-10-19 06:57:25 +0000

    SQL script

--- resource/SAD_testSAD.sql
+++ resource/SAD_testSAD.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `SAD` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
-USE `SAD`;
+CREATE DATABASE  IF NOT EXISTS `cungonbai_data` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `cungonbai_data`;
 -- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
 --
--- Host: 128.199.209.247    Database: SAD
+-- Host: 128.199.209.247    Database: cungonbai_data
 -- ------------------------------------------------------
 -- Server version	5.5.35-0ubuntu0.12.04.2
 
@@ -18,27 +18,198 @@ USE `SAD`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `testSAD`
+-- Table structure for table `tbl_answer`
 --
 
-DROP TABLE IF EXISTS `testSAD`;
+DROP TABLE IF EXISTS `tbl_answer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `testSAD` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `testValue` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `tbl_answer` (
+  `answer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `point` int(11) NOT NULL COMMENT 'Value from -100 to 100',
+  PRIMARY KEY (`answer_id`),
+  KEY `question_id` (`question_id`),
+  CONSTRAINT `tbl_answer_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `tbl_question` (`question_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `testSAD`
+-- Dumping data for table `tbl_answer`
 --
 
-LOCK TABLES `testSAD` WRITE;
-/*!40000 ALTER TABLE `testSAD` DISABLE KEYS */;
-INSERT INTO `testSAD` VALUES (1,'test1'),(2,'test2'),(3,'test3');
-/*!40000 ALTER TABLE `testSAD` ENABLE KEYS */;
+LOCK TABLES `tbl_answer` WRITE;
+/*!40000 ALTER TABLE `tbl_answer` DISABLE KEYS */;
+INSERT INTO `tbl_answer` VALUES (1,1,'toi la khang',100),(2,1,'toi la trung',0),(3,1,'toi la hoa',0),(4,1,'toi la hoang',0);
+/*!40000 ALTER TABLE `tbl_answer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_favorite`
+--
+
+DROP TABLE IF EXISTS `tbl_favorite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_favorite` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `question_id` (`question_id`),
+  CONSTRAINT `tbl_favorite_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tbl_favorite_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `tbl_question` (`question_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_favorite`
+--
+
+LOCK TABLES `tbl_favorite` WRITE;
+/*!40000 ALTER TABLE `tbl_favorite` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_favorite` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_question`
+--
+
+DROP TABLE IF EXISTS `tbl_question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_question` (
+  `question_id` int(11) NOT NULL AUTO_INCREMENT,
+  `subject_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`question_id`),
+  KEY `subject_id` (`subject_id`),
+  CONSTRAINT `tbl_question_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `tbl_subject` (`subject_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_question`
+--
+
+LOCK TABLES `tbl_question` WRITE;
+/*!40000 ALTER TABLE `tbl_question` DISABLE KEYS */;
+INSERT INTO `tbl_question` VALUES (1,1,'Cau hoi 1 : toi la ai');
+/*!40000 ALTER TABLE `tbl_question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_session`
+--
+
+DROP TABLE IF EXISTS `tbl_session`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_session` (
+  `session_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `ended_date` int(11) NOT NULL COMMENT 'second',
+  `started_date` int(11) NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `user_id` (`user_id`),
+  KEY `subject_id` (`subject_id`),
+  CONSTRAINT `tbl_session_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tbl_session_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `tbl_subject` (`subject_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_session`
+--
+
+LOCK TABLES `tbl_session` WRITE;
+/*!40000 ALTER TABLE `tbl_session` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_session` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_session_question`
+--
+
+DROP TABLE IF EXISTS `tbl_session_question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_session_question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `answer_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `session_id` (`session_id`,`question_id`,`answer_id`),
+  KEY `session_id_2` (`session_id`),
+  KEY `question_id` (`question_id`),
+  KEY `answer_id` (`answer_id`),
+  CONSTRAINT `tbl_session_question_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `tbl_question` (`question_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tbl_session_question_ibfk_3` FOREIGN KEY (`answer_id`) REFERENCES `tbl_answer` (`answer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tbl_session_question_ibfk_4` FOREIGN KEY (`session_id`) REFERENCES `tbl_session` (`session_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_session_question`
+--
+
+LOCK TABLES `tbl_session_question` WRITE;
+/*!40000 ALTER TABLE `tbl_session_question` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_session_question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_subject`
+--
+
+DROP TABLE IF EXISTS `tbl_subject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_subject` (
+  `subject_id` int(11) NOT NULL AUTO_INCREMENT,
+  `subject_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`subject_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_subject`
+--
+
+LOCK TABLES `tbl_subject` WRITE;
+/*!40000 ALTER TABLE `tbl_subject` DISABLE KEYS */;
+INSERT INTO `tbl_subject` VALUES (1,'SAD');
+/*!40000 ALTER TABLE `tbl_subject` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_user`
+--
+
+DROP TABLE IF EXISTS `tbl_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `score` int(11) NOT NULL,
+  `is_admin` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+LOCK TABLES `tbl_user` WRITE;
+/*!40000 ALTER TABLE `tbl_user` DISABLE KEYS */;
+INSERT INTO `tbl_user` VALUES (5,'qwe213',0,0),(6,'ertert',0,0),(7,'xvbxcb',0,0);
+/*!40000 ALTER TABLE `tbl_user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -50,4 +221,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-10-10 16:20:30
+-- Dump completed on 2014-10-19 13:56:26
