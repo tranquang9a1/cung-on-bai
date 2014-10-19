1a9f2bc5947571610b7f762217b4917a95e048cc
truongnguyenbao <truongnguyenbao@gmail.com>
2014-10-19 04:28:06 +0000

    Update AnswerServlet QuestionServlet TblUser

--- code/ProjectSAD-Sam/src/java/dao/AnswerDao.java
+++ code/ProjectSAD-Sam/src/java/dao/AnswerDao.java
@@ -5,11 +5,19 @@
 package dao;
 
 import entity.TblAnswer;
+import entity.TblQuestion;
+import java.util.List;
+import javax.persistence.Query;
 
 /**
  *
  * @author khangtnse60992
  */
 public class AnswerDao extends BaseDao<TblAnswer>{
-    
+    public List<TblAnswer> getListAnswer(TblQuestion questionId) {
+        Query query = null;
+        query = em.createNamedQuery("TblAnswer.findAllByQuestionId");
+        query.setParameter("questionId", questionId);
+        return query.getResultList();      
+    }
 }
