04941b9e5f8ec5ab52bd7d665ba30deff187ea10
hoaptt93 <hoaptt93@gmail.com>
2014-10-20 15:36:35 +0000

    update

---
+++ code/ProjectSAD-Sam/src/java/dao/SessionDao.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entity.TblSession;
+
+/**
+ *
+ * @author computer
+ */
+public class SessionDao extends BaseDao<TblSession>{
+    
+}
---
+++ code/ProjectSAD-Sam/src/java/dao/SessionQuestionDao.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entity.TblSessionQuestion;
+
+/**
+ *
+ * @author computer
+ */
+public class SessionQuestionDao extends BaseDao<TblSessionQuestion>{
+    
+}
