918017976a2bc579df938219d6143c42aaa1cc08
truongnguyenbao <truongnguyenbao@gmail.com>
2014-10-21 15:22:31 +0000

    Fix bug Update question

--- code/ProjectSAD-Sam/src/java/dao/AnswerDao.java
+++ code/ProjectSAD-Sam/src/java/dao/AnswerDao.java
@@ -15,16 +15,18 @@ import javax.persistence.Query;
  *
  * @author khangtnse60992
  */
-public class AnswerDao extends BaseDao<TblAnswer>{
+public class AnswerDao extends BaseDao<TblAnswer> {
+
     public List<TblAnswer> getListAnswer(TblQuestion questionId) {
         Query query = null;
         query = em.createNamedQuery("TblAnswer.findAllByQuestionId");
         query.setParameter("questionId", questionId);
-        return query.getResultList();      
+        return query.getResultList();
     }
-    public void insertAnswer(int[] point,String[] content , TblQuestion tblQuestion) {
+
+    public void insertAnswer(int[] point, String[] content, TblQuestion tblQuestion) {
         TblAnswer answer = null;
-        for(int i = 0 ; i < content.length; i++) {
+        for (int i = 0; i < content.length; i++) {
             answer = new TblAnswer();
             answer.setContent(content[i]);
             answer.setPoint(point[i]);
@@ -32,28 +34,48 @@ public class AnswerDao extends BaseDao<TblAnswer>{
             insert(answer);
         }
     }
-    
+
     public TblAnswer getAnswerById(int id) {
         Query query = em.createNamedQuery("TblAnswer.findByAnswerId");
         return (TblAnswer) query.getSingleResult();
     }
-    
-    public List<TblAnswer> insertListAnswer(int[] point,String[] content , TblQuestion tblQuestion) {
+
+    public List<TblAnswer> getListAnswer(int[] point, String[] content, TblQuestion tblQuestion) {
         List<TblAnswer> tblAnswers = new ArrayList<TblAnswer>();
         TblAnswer answer = null;
-        for(int i = 0 ; i < content.length; i++) {
+        for (int i = 0; i < content.length; i++) {
             answer = new TblAnswer();
             answer.setContent(content[i]);
             answer.setPoint(point[i]);
             answer.setQuestionId(tblQuestion);
             tblAnswers.add(answer);
         }
-        return  tblAnswers;
+        return tblAnswers;
     }
-    public void updateAnswer(int[] id,int[] point,String[] content , TblQuestion tblQuestion) {
+
+    public List<TblAnswer> getListAnswer(int[] id, int[] point, String[] content, TblQuestion tblQuestion) {
         TblAnswer answer = null;
-        
-        for(int i = 0 ; i < id.length; i++) {
+        List<TblAnswer> tblAnswers = new ArrayList<TblAnswer>();
+        for (int i = 0; i < content.length; i++) {
+            try {
+                answer = findById(TblAnswer.class, id[i]);
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                e.printStackTrace();
+                answer = new TblAnswer();
+            }
+            answer.setContent(content[i]);
+            answer.setPoint(point[i]);
+            answer.setQuestionId(tblQuestion);
+            tblAnswers.add(answer);
+        }
+        return tblAnswers;
+    }
+
+    public void updateAnswer(int[] id, int[] point, String[] content, TblQuestion tblQuestion) {
+        TblAnswer answer = null;
+
+        for (int i = 0; i < id.length; i++) {
             answer = findById(TblAnswer.class, id[i]);
             answer.setContent(content[i]);
             answer.setPoint(point[i]);
@@ -61,7 +83,8 @@ public class AnswerDao extends BaseDao<TblAnswer>{
             update(answer);
         }
     }
-   public TblAnswer findWrongAnswerByQuestionId(int questionId) {
+
+    public TblAnswer findWrongAnswerByQuestionId(int questionId) {
         Query query = em.createNamedQuery("TblAnswer.findWrongAnswerByQuestionId");
         query.setParameter("questionId", questionId);
         return (TblAnswer) query.getResultList().get(0);
