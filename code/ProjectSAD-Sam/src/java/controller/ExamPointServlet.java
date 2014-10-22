e77f2b13304b35759c85aed27c0cc4773216c219
hoaptt93 <hoaptt93@gmail.com>
2014-10-22 03:11:10 +0000

    add

---
+++ code/ProjectSAD-Sam/src/java/controller/ExamPointServlet.java
@@ -0,0 +1,113 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import entity.TblSession;
+import entity.TblSessionQuestion;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import utils.Constants;
+
+/**
+ *
+ * @author computer
+ */
+public class ExamPointServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        TblSession examSession = (TblSession) request.getAttribute("examSession");
+        List<TblSessionQuestion> lst = examSession.getTblSessionQuestionList();
+        //create map to store point of each question
+        Map<Integer, Integer> mapPoint = new HashMap<Integer, Integer>();
+        for (int i = 0; i < lst.size(); i++) {
+            TblSessionQuestion sessionQuestion = lst.get(i);
+            mapPoint.put(sessionQuestion.getQuestionId().getQuestionId(), 0);
+        }
+        // canculate point for each question
+        for (int i = 0; i < lst.size(); i++) {
+            TblSessionQuestion sessionQuestion = lst.get(i);
+            for (Entry entry : mapPoint.entrySet()) {
+                Integer key = (Integer) entry.getKey();
+                if (sessionQuestion.getQuestionId().getQuestionId() == key) {
+                    if (sessionQuestion.getAnswerId() != null) { // check answer null or not
+                        Integer value = (Integer) entry.getValue() + sessionQuestion.getAnswerId().getPoint();
+                        mapPoint.put(key, value);
+                    }
+                }
+            }
+        }
+        // canculate total point
+        int pointSum = 0;
+        for (Entry entry : mapPoint.entrySet()) {
+            Integer value = (Integer) entry.getValue();
+            if (value > 0) {
+                pointSum += value;
+            }
+        }
+        request.setAttribute("numberQuestion", mapPoint.size());
+        request.setAttribute("point", pointSum / 100);
+        request.getRequestDispatcher(Constants.JSP_VIEWPOINT).forward(request, response);
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
