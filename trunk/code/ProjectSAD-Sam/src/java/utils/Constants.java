e851277b332a2dd0a2799aa4e426939b234d6c95
hoaptt93@gmail.com <hoaptt93@gmail.com@dae60883-eef8-82d8-cb82-88d7675c4593>
2014-10-22 16:58:56 +0000

    update

--- trunk/code/ProjectSAD-Sam/src/java/utils/Constants.java
+++ trunk/code/ProjectSAD-Sam/src/java/utils/Constants.java
@@ -17,12 +17,16 @@ public class Constants {
     public static final String JSP_HOME = "WEB-INF/common/home.jsp";
     public static final String JSP_LOGIN = "WEB-INF/user/login.jsp";
     public static final String URL_ADMIN = "admin";
-    public static final String JSP_ADMIN = "WEB-INF/user/admin.jsp"; 
+    public static final String JSP_ADMIN = "WEB-INF/user/admin.jsp";
     public static final String URL_SUBJECT = "SubjectServlet";
     public static final String JSP_SUBJECT = "WEB-INF/subject/createSubject.jsp";
     public static final String JSP_CREATESUBJECT = "WEB-INF/subject/createSubject.jsp";
     public static final String JSP_EXAMSESSION = "WEB-INF/exam/examSession.jsp";
     public static final String URL_POINT = "point";
-     public static final String JSP_VIEWPOINT = "WEB-INF/exam/viewPoint.jsp";
+    public static final String JSP_VIEWPOINT = "WEB-INF/exam/viewPoint.jsp";
     public static final String JSP_STAT = "WEB-INF/user/stat.jsp";
+    public static final String CONST_MESSAGE = "message";
+    public static final String CONST_BACKURL = "backUrl";
+    public static final String MESSAGE_WRONGINPUT = "Bạn đã nhập sai thông tin. Vui lòng kiểm tra lại";
+    
 }
