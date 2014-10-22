6e10e99d49a7f99ca087ac5c4e1a8923730ac7db
hoaptt93@gmail.com <hoaptt93@gmail.com@dae60883-eef8-82d8-cb82-88d7675c4593>
2014-10-22 17:30:21 +0000

    update

--- trunk/code/ProjectSAD-Sam/src/java/controller/ExamPointServlet.java
+++ trunk/code/ProjectSAD-Sam/src/java/controller/ExamPointServlet.java
@@ -4,8 +4,10 @@
  */
 package controller;
 
+import dao.UserDao;
 import entity.TblSession;
 import entity.TblSessionQuestion;
+import entity.TblUser;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -15,6 +17,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import utils.Constants;
 
 /**
@@ -35,39 +38,31 @@ public class ExamPointServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        TblSession examSession = (TblSession) request.getAttribute("examSession");
-        List<TblSessionQuestion> lst = examSession.getTblSessionQuestionList();
-        //create map to store point of each question
-        Map<Integer, Integer> mapPoint = new HashMap<Integer, Integer>();
-        for (int i = 0; i < lst.size(); i++) {
-            TblSessionQuestion sessionQuestion = lst.get(i);
-            mapPoint.put(sessionQuestion.getQuestionId().getQuestionId(), 0);
-        }
-        // canculate point for each question
-        for (int i = 0; i < lst.size(); i++) {
-            TblSessionQuestion sessionQuestion = lst.get(i);
+        HttpSession session = request.getSession(true);
+        TblUser user = (TblUser) session.getAttribute(Constants.VAR_SESSION_USER);
+        if (user == null) {
+            response.sendRedirect(Constants.URL_USER);
+        } else {
+            TblSession examSession = (TblSession) request.getAttribute("examSession");
+            List<TblSessionQuestion> lst = examSession.getTblSessionQuestionList();
+            // canculate total point
+            Map<Integer, Integer> mapPoint = canculatePoint(lst);
+            int pointSum = 0;
             for (Entry entry : mapPoint.entrySet()) {
-                Integer key = (Integer) entry.getKey();
-                if (sessionQuestion.getQuestionId().getQuestionId() == key) {
-                    if (sessionQuestion.getAnswerId() != null) { // check answer null or not
-                        Integer value = (Integer) entry.getValue() + sessionQuestion.getAnswerId().getPoint();
-                        mapPoint.put(key, value);
-                    }
+                Integer value = (Integer) entry.getValue();
+                if (value > 0) {
+                    pointSum += value;
                 }
             }
+            request.setAttribute("numberQuestion", mapPoint.size());
+            double point = (double) (pointSum) / 100.00;
+            request.setAttribute("point", point);
+            // add point into account of user
+            long pointLong = Math.round(point);
+            int pointInt = (int) pointLong;
+            user.setScore(user.getScore() + pointInt);
+            request.getRequestDispatcher(Constants.JSP_VIEWPOINT).forward(request, response);
         }
-        // canculate total point
-        int pointSum = 0;
-        for (Entry entry : mapPoint.entrySet()) {
-            Integer value = (Integer) entry.getValue();
-            if (value > 0) {
-                pointSum += value;
-            }
-        }
-        request.setAttribute("numberQuestion", mapPoint.size());
-        double point = (double)(pointSum) / 100.00;
-        request.setAttribute("point", point);
-        request.getRequestDispatcher(Constants.JSP_VIEWPOINT).forward(request, response);
 
     }
 
@@ -111,4 +106,27 @@ public class ExamPointServlet extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+    private Map<Integer, Integer> canculatePoint(List<TblSessionQuestion> lst) {
+        //create map to store point of each question
+        Map<Integer, Integer> mapPoint = new HashMap<Integer, Integer>();
+        for (int i = 0; i < lst.size(); i++) {
+            TblSessionQuestion sessionQuestion = lst.get(i);
+            mapPoint.put(sessionQuestion.getQuestionId().getQuestionId(), 0);
+        }
+        // canculate point for each question
+        for (int i = 0; i < lst.size(); i++) {
+            TblSessionQuestion sessionQuestion = lst.get(i);
+            for (Entry entry : mapPoint.entrySet()) {
+                Integer key = (Integer) entry.getKey();
+                if (sessionQuestion.getQuestionId().getQuestionId() == key) {
+                    if (sessionQuestion.getAnswerId() != null) { // check answer null or not
+                        Integer value = (Integer) entry.getValue() + sessionQuestion.getAnswerId().getPoint();
+                        mapPoint.put(key, value);
+                    }
+                }
+            }
+        }
+        return mapPoint;
+    }
 }
